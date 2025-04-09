584d5ef96cd52ee4f2b3991c2ab8663c076578ad
Srireddy88 <lakshmisri0805@gmail.com>
2025-04-09 21:19:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello from Jenkins CI/CD pipeline!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
 });
